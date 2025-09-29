c062a00c56fa5d6892fad41ce85564211ceec171
ledusantos <ledu.borges.santos@gmail.com>
2025-09-29 20:54:28 -0300

    Arquivos da aula01

---
+++ aula01_bootcamp/exemplo_00.py
@@ -0,0 +1,5 @@
+
+# Crie um programa que o usuário digita o seu nome e retona o número de caracteres
+nome = input("Digite seu nome:")
+tamanho = len(nome)
+print(f"O numero de letras no seu nome é {tamanho}")
---
+++ aula01_bootcamp/exemplo_01.py
@@ -0,0 +1,6 @@
+#Crie um programa onde o usuário digite dois valores e aplique a soma
+
+num1 = int(input("Digite o primeiro numero:"))
+num2 = int(input("Digite o segundo numero:"))
+soma = num1 + num2
+print(f"A soma dos números é: {soma}")
